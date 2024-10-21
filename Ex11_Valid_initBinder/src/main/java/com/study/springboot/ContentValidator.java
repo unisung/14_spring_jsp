d8d2d827932319842bc4f89d076f2e1106501ca5
unisung <vctor@naver.com>
2024-10-21 11:37:35 +0900

    ex11

---
+++ Ex11_Valid_initBinder/src/main/java/com/study/springboot/ContentValidator.java
@@ -0,0 +1,39 @@
+package com.study.springboot;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+//spring에서 제공해주는 검증(입력값의 null여부, 타입)객체
+public class ContentValidator implements  Validator {
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return ContentDto.class.isAssignableFrom(clazz) ;
+	}//검증할 객체 클래스 타입정보
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ContentDto dto = (ContentDto)target;
+		
+//		String sWriter = dto.getWriter();
+		// trim("  aaa ") -> "aaa", trim("    ") -> ""  
+//		if(sWriter  == null || sWriter.trim().isEmpty()) {
+//			System.out.println("Writer is null or empty");
+//			errors.rejectValue("writer", "trouble");//전달할 오류정보
+//		}
+	ValidationUtils.rejectIfEmptyOrWhitespace(errors,
+				"writer",
+				"Writer is null or empty");
+		
+		
+//		String sContent = dto.getContent();
+//		if(sContent == null || sContent.trim().isEmpty()) {
+//			System.out.println("Content is null or empty");
+//			errors.rejectValue("content", "trouble");
+//		}
+	ValidationUtils.rejectIfEmptyOrWhitespace(errors, 
+			"content", 
+			"Content is null or empty");
+	
+	}
+}
---
+++ Ex11_Valid_initBinder/src/main/java/com/study/springboot/Ex11ValidInitBinderApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex11ValidInitBinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex11ValidInitBinderApplication.class, args);
+	}
+
+}
