35293cbf007f2ae7846ba03f8ecd52ffac2f10e0
unisung <vctor@naver.com>
2024-10-28 10:32:27 +0900

    1028-1. Ex20_start

---
+++ Ex20_SqlLog/src/main/java/com/study/springboot/UserController.java
@@ -0,0 +1,45 @@
+package com.study.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.study.springboot.dao.IDao;
+import com.study.springboot.dto.UserDTO;
+
+@Controller
+public class UserController {
+	@Autowired
+	private IDao dao;
+	
+  @RequestMapping("/employees")
+  public String userlistPage(Model model) {
+	  List<UserDTO> list = dao.getEmployee();
+	  for(UserDTO dto: list) {
+		  System.out.println(dto);
+	  }
+	  model.addAttribute("employees", list);
+	  
+	  return "employeelist";
+  }
+  
+  //localhost:8787/employee/7782
+  @GetMapping("/employee/{empno}") 
+  public String empInfo(@PathVariable("empno") String empno,
+		  Model model) {
+	  System.out.println("empno:"+empno);
+	  //dao로 empno에 해당하는 정보를 요청
+	  UserDTO emp= dao.getEmpInfo(empno);
+	  //정보출력
+	  System.out.println("empInfo:"+emp);
+	  
+	  model.addAttribute("empInfo", emp);
+	  return "employee";//employee.jsp
+  }
+  
+}
---
+++ Ex20_SqlLog/src/main/webapp/WEB-INF/views/writeForm.jsp
@@ -0,0 +1,32 @@
+<%@ page pageEncoding="UTF-8"%>
+<!DOCTYPE html><html><head>
+<meta charset="UTF-8">
+<title>글작성 페이지</title>
+</head>
+<body>
+<br><p>
+<table width="500" border="1" cellpadding="0"
+          cellspacing="0">
+<form action="write" method="post">
+  <tr>
+   <td>작성자</td>
+   <td><input name="writer" size="20"></td>
+  </tr>
+  <tr>
+   <td>제목</td>
+   <td><input name="title" size="100"></td>
+  </tr>
+  <tr>
+   <td>내용</td>
+   <td><input name="content" size="100"></td>
+  </tr>
+  <tr>
+   <td colspan="2">
+   	<input type="submit" value="입력"> &nbsp;&nbsp;
+   	<a href="/list">목록보기</a>
+   </td>
+  </tr>
+</form>
+</table>
+</body>
+</html>
