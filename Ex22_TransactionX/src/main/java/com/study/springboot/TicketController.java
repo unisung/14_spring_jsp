a943ca62aed9404d8e9f0717e0d47d163df60f86
unisung <vctor@naver.com>
2024-10-29 12:22:22 +0900

    1029-3.

--- Ex22_TransactionX/src/main/java/com/study/springboot/TicketController.java
+++ Ex22_TransactionX/src/main/java/com/study/springboot/TicketController.java
@@ -2,7 +2,9 @@ package com.study.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.springboot.service.IBuyTicketService;
 
@@ -14,6 +16,26 @@ public class TicketController {
 	@RequestMapping("/buy_ticket")
 	public String buy_ticket() {
 		return "buy_ticket";
+	}
+	
+	@RequestMapping("/buy_ticket_card")
+	public String buy_ticket_card(
+			@RequestParam("consumerId") String consumerId,
+			@RequestParam("amount") int amount,//String -> int
+			@RequestParam("error") String error,
+			Model model
+			) {
+		int nResult = buyTicket.buy(consumerId,amount, error);
+		
+		model.addAttribute("consumerId", consumerId);
+		model.addAttribute("amount", amount);
+		
+		if(nResult == 1) {
+			return "buy_ticket_end";
+		}else {
+			return "buy_ticket_error";
+		}
+		
 	}
 
 }
