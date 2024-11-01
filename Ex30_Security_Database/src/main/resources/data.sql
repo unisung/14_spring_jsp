dc4fd6157b9b5762aed66204a3cd4e8de5a95f56
unisung <vctor@naver.com>
2024-11-01 10:21:04 +0900

    1101-Se-Db

---
+++ Ex30_Security_Database/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_list values('user', '암호화된 패스워드','ROLE_USER',1);
+insert into user_list values('admin', '암호화된 패스워드','ROLE_ADMIN',1);
+commit;
---
+++ Ex30_Security_Database/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table user_list(
+name varchar2(20) primary key,
+password varchar2(100),
+authority varchar2(20),
+enabled number(1)
+);
