c7e49ab37a78527563f974519696e098b0103029
unisung <vctor@naver.com>
2024-11-01 11:25:58 +0900

    1101-5.Security_Database-end.

--- Ex30_Security_Database/src/main/java/com/study/springboot/Ex30SecurityDatabaseApplication.java
+++ Ex30_Security_Database/src/main/java/com/study/springboot/Ex30SecurityDatabaseApplication.java
@@ -2,12 +2,19 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Ex30SecurityDatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex30SecurityDatabaseApplication.class, args);
+		
+		String encoded=PasswordEncoderFactories
+				.createDelegatingPasswordEncoder()
+				.encode("1234");
+		System.out.println(encoded);
 	}
 
 }
