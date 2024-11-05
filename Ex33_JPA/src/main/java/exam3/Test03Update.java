5e022b5ae38888240f2ce5fb8f1ecf6c3e2b15c1
unisung <vctor@naver.com>
2024-11-05 11:49:51 +0900

    1105-7.Scanner

--- Ex33_JPA/src/main/java/exam3/Test03Update.java
+++ Ex33_JPA/src/main/java/exam3/Test03Update.java
@@ -1,22 +1,32 @@
 package exam3;
 
-import java.time.LocalDate;
+import java.util.Scanner;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Test03Update {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		EntityManagerFactory emf =
 		   Persistence.createEntityManagerFactory("JpaEx01");
 		EntityManager em
 		          = emf.createEntityManager();
 		
 		try {
+			
+			 System.out.println("수정하려는 email?:");
+			 String email = scanner.nextLine();
+			 
+			 System.out.println("바꿀려는 이름?:");
+			 String name = scanner.nextLine();
+			 
+			 
 			 em.getTransaction().begin();
 			
 			Member3 user 
-			    = em.find(Member3.class, "test@test.com");
+			    = em.find(Member3.class, email);
 			if(user==null) {
 				System.out.println("존재하지 않습니다.");
 				em.getTransaction().rollback();
@@ -24,14 +34,14 @@ public class Test03Update {
 			}
 			
 			//수정처리
-			user.changeName("전우치");//DB가 아닌 영속컨텍스에 변경
+			user.changeName(name);//DB가 아닌 영속컨텍스에 변경
 			
 			em.getTransaction().commit();//DB반영
 
 			System.out.println("이름을 변경했습니다.");
 			//수정내용 확인
 			Member3 userDB = 
-					em.find(Member3.class, "test@test.com");
+					em.find(Member3.class, email);
 			
 			System.out.println("["+userDB+"]");
 			System.out.println("이름:"+userDB.getName());
