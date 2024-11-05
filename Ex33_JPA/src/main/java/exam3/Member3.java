ff5a5ab7e85d56bb165d2d9d5ed3917b78dba85b
unisung <vctor@naver.com>
2024-11-05 10:55:40 +0900

    1105-4.

--- Ex33_JPA/src/main/java/exam3/Member3.java
+++ Ex33_JPA/src/main/java/exam3/Member3.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
--- Ex33_JPA/src/main/java/exam3/Test01Insert.java
+++ Ex33_JPA/src/main/java/exam3/Test01Insert.java
@@ -20,9 +20,13 @@ public class Test01Insert {
 			
 			Member3 user 
 			    = new Member3("test@test.com","홍길동2",LocalDate.now());
+			System.out.println(111);
 			em.persist(user);//저장메소드
+			System.out.println(222);
 			
 			em.getTransaction().commit();//DB반영
+			System.out.println(333);
+			System.out.println("가입 요청을 처리했습니다.");
 			
 		}catch(Exception e) {
 			 e.printStackTrace();
