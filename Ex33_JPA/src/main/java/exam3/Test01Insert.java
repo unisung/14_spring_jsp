0aa824375f9fbccd7f5e5dfc915735763aa6a543
unisung <vctor@naver.com>
2024-11-05 11:43:41 +0900

    1105-7.insert 받기

--- Ex33_JPA/src/main/java/exam3/Test01Insert.java
+++ Ex33_JPA/src/main/java/exam3/Test01Insert.java
@@ -2,6 +2,7 @@ package exam3;
 
 import java.beans.PersistenceDelegate;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -10,6 +11,8 @@ import jakarta.persistence.Persistence;
 
 public class Test01Insert {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
 		EntityManagerFactory emf =
 		   Persistence.createEntityManagerFactory("JpaEx01");
 		EntityManager em
@@ -18,8 +21,18 @@ public class Test01Insert {
 		try {
 			 em.getTransaction().begin();
 			
+			System.out.println("입력하려는 이메일?:");
+			String email = scanner.nextLine();
+			
+			System.out.println("입력하려는 이름?:");
+			String name = scanner.nextLine();
+				
+			System.out.println("이메일확인: " + email);
+			System.out.println("이름확인: " + name);	
+				
 			Member3 user 
-			    = new Member3("test@test.com","홍길동2",LocalDate.now());
+			    = new Member3(email,name,LocalDate.now());
+			
 			System.out.println(111);
 			em.persist(user);//저장메소드
 			System.out.println(222);
