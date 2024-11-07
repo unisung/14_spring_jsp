b773a0e4c4c1808d56535a14ca79a7d97428c2c6
unisung <vctor@naver.com>
2024-11-07 10:32:58 +0900

    1107-03.Service,

--- Ex35_SpringJpa02/src/main/java/com/study/springboot/jpa/MemberService.java
+++ Ex35_SpringJpa02/src/main/java/com/study/springboot/jpa/MemberService.java
@@ -8,37 +8,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-	
 	@Autowired
 	private MemberRepository memberRepository;
 	//저장
-	public Member insert(Member member) {
-		Member returnMember = memberRepository.save(member);
-		return returnMember;
+	public void insert() {
+	Member member;
+    member = Member.builder().name("이순신")
+    		      .email("test1@test.com").build();
+    memberRepository.save(member);
+    
 	}
+	
 	//한 건 조회
-	public Optional<Member> select(Long id){
-		Optional<Member> member 
-		           = memberRepository.findById(id);
-		return member;
-	}
-	//여러건 조회
-	public List<Member> selectAll(){
-		return memberRepository.findAll();
-	}
-	
-	//삭제
-	public void delete(Long id) {
-		memberRepository.deleteById(id);
-	}
-	
-	//수정
-	public Member update(Member member) {
-		System.out.println("update:"+member);
-		//키 값과 같은 객체가 있으면 업데이트, 키 값과 같은 객체가 없으면 인서트
-		Member returnMember = memberRepository.save(member);
-		return returnMember;
-	}
-	
 
 }
