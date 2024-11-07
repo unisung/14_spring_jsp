c2c7d493d87d67004d0303e5ab2c623d78198d58
unisung <vctor@naver.com>
2024-11-07 10:35:43 +0900

    1107-004.Service-insert

--- Ex35_SpringJpa02/src/main/java/com/study/springboot/jpa/MemberService.java
+++ Ex35_SpringJpa02/src/main/java/com/study/springboot/jpa/MemberService.java
@@ -16,7 +16,30 @@ public class MemberService {
     member = Member.builder().name("이순신")
     		      .email("test1@test.com").build();
     memberRepository.save(member);
-    
+    member = Member.builder().name("강감찬")
+		      .email("test2@test.com").build();
+    memberRepository.save(member);
+    member = Member.builder().name("을지문덕")
+		      .email("test3@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("계백")
+		      .email("test4@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("김유신")
+		      .email("test5@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("연개소문")
+		      .email("test6@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("양만춘")
+		      .email("test7@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("김종서")
+		      .email("test8@test.com").build();
+	memberRepository.save(member);
+	member = Member.builder().name("최영")
+		      .email("test9@test.com").build();
+	memberRepository.save(member);
 	}
 	
 	//한 건 조회
