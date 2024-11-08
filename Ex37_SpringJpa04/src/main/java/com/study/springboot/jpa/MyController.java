66250790bb217a2a93bbf548faedc6ac354b1d72
unisung <vctor@naver.com>
2024-11-08 12:19:13 +0900

    1108-6. jsp

--- Ex37_SpringJpa04/src/main/java/com/study/springboot/jpa/MyController.java
+++ Ex37_SpringJpa04/src/main/java/com/study/springboot/jpa/MyController.java
@@ -22,42 +22,21 @@ public class MyController {
 		return "menu"; //menu.jsp로 forward
 	}
 
-	@RequestMapping("/selectByNameLike")
-	public String selectByNameLike(@RequestParam("name") String search,
-			                      @RequestParam("page") String page,
-			                      Model model){
-		System.out.println("***" + search + "***");
-		System.out.println("***" + page + "***");
+	@RequestMapping("/selectByNameLike1")
+	public String selectByNameLike(@RequestParam("name") String search,  
+			                       Model model){
+		
+		System.out.println("***1" + search + "***");
 		
 		String name = "%" + search + "%";
-	    Sort sort 
-	   = Sort.by(Sort.Order.desc("name"));//정렬기준칼럼-"name"
-	    int nPage = Integer.parseInt(page) -1;	//page시작 0
-	    
-	    // 페이지는 0부터
-	    Pageable pageable = PageRequest.ofSize(10)//페이지당 글 개수
-	    		                       .withPage(nPage)//현재페이지-1
-	    		                       .withSort(sort);//정렬기준
-	    
-	    //위에서 생성한 페이지정보와 조회조건으로 조회결과를 Page에 담기
-	    Page<Member> result 
-	       = memberService.selectNameLike(name, pageable);
-	    List<Member> content = result.getContent();
-	    long totalElements = result.getTotalElements();
-	    int totalPages = result.getTotalPages();
-	    int size = result.getSize();
-	    int pageNumber = result.getNumber() + 1;//!!!
-	    int numberOfElements = result.getNumberOfElements();//콘텐츠 갯수
-	    
+	   
+	    List<Member> result 
+	              = memberService.selectMembers1(name);
+	   
 	    //모델에 저장하여 forward되는 페이지로 전달
-	    model.addAttribute("members", content);
-	    model.addAttribute("totalEements", totalElements);
-	    model.addAttribute("totalPages", totalPages);
-	    model.addAttribute("size", size);
-	    model.addAttribute("pageNumber", pageNumber);
-	    model.addAttribute("numberOfElements", numberOfElements);
+	    model.addAttribute("members", result);
 	    
-		return "select_name_list";//select_name_list.jsp로 forward.
+		return "select_name_list_1";//select_name_list_1.jsp로 forward.
 		
 	}
 }
--- Ex37_SpringJpa04/src/main/webapp/WEB-INF/views/menu.jsp
+++ Ex37_SpringJpa04/src/main/webapp/WEB-INF/views/menu.jsp
@@ -4,18 +4,18 @@
 <title>메뉴페이지</title>
 </head>
 <body>
- <%="Spring JPA #03" %>
+ <%="Spring JPA #04" %>
  <br><p>
  
- <a href="/selectByNameLike?name=test&page=1">
-  Name Like 조회 : 1페이지</a><br><p>
- <a href="/selectByNameLike?name=test&page=2">
-  Name Like 조회 : 2페이지</a><br><p>
- <a href="/selectByNameLike?name=test&page=3">
-  Name Like 조회 : 3페이지</a><br><p>
+ <a href="/selectByNameLike1?name=test">
+  Name Like 조회 : JPQL 1</a><br><p>
+ <a href="/selectByNameLike2?name=test">
+  Name Like 조회 : JPQL 1</a><br><p>
+ <a href="/selectByNameLike3?name=test&page=2">
+  Name Like 조회 : JPQL 3 - 2페이지</a><br><p>
  <hr>
- <a href="/selectByNameLike?name=test&page=4">
-  Name Like 조회 : 4페이지</a><br><p>
+ <a href="/selectByNameLike4?name=test">
+  Name Like 조회 : Native SQL</a><br><p>
  
  
 </body>
