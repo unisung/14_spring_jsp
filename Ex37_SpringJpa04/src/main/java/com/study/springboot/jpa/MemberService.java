cb2bd8bdb337c78ecb76175bc41c67cb0124b454
unisung <vctor@naver.com>
2024-11-08 12:09:46 +0900

    1108-5. Service.

--- Ex37_SpringJpa04/src/main/java/com/study/springboot/jpa/MemberService.java
+++ Ex37_SpringJpa04/src/main/java/com/study/springboot/jpa/MemberService.java
@@ -1,8 +1,10 @@
 package com.study.springboot.jpa;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +12,24 @@ public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 	
-	public Page<Member>
-	selectNameLike(String search, Pageable pageable){
-	return memberRepository.findByNameLike(search, 
-			                          pageable);
+	public List<Member> selectMembers1(String search){
+		return memberRepository.findMembers(search);
 	}
+	
+	public List<Member> selectMembers2(String search, 
+			                          Sort sort){
+		return memberRepository.findMembers(search,sort);
+	}
+	
+	public List<Member> selectMembers3(String search, 
+			                       Pageable pageable){
+		return memberRepository.findMembers(search,pageable);
+	}
+	
+	public List<Member> selectMembers4(String search){
+		return memberRepository.findMembersNative(search);
+	}
+	
+	
+	
 }
