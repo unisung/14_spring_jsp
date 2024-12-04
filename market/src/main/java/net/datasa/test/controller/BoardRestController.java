3da4d5333d1daf0ef8b0cdcff898af93baadab27
unisung <vctor@naver.com>
2024-12-04 18:00:20 +0900

    11.04- 구매 및 reply처리완료

--- market/src/main/java/net/datasa/test/controller/BoardRestController.java
+++ market/src/main/java/net/datasa/test/controller/BoardRestController.java
@@ -3,6 +3,8 @@ package net.datasa.test.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.datasa.test.domain.dto.BoardDTO;
+import net.datasa.test.domain.dto.ReplyDTO;
+import net.datasa.test.security.AuthenticatedUser;
 import net.datasa.test.service.BoardService;
 
 /**
@@ -40,6 +44,46 @@ public class BoardRestController {
 		  = boardService.getList(category, keyword);
 		
 		return ResponseEntity.ok(list);//status-200, 결과값-list		
+	}
+	
+	/**
+     * 리플 저장
+     * @param replyDTO 저장할 리플 정보
+     * @param user      로그인한 사용자 정보
+     */
+	@PostMapping("replyWrite")
+	public void replyWrite(ReplyDTO replyDTO,
+			@AuthenticationPrincipal AuthenticatedUser user) {
+		replyDTO.setMemberId(user.getUsername());
+		
+		boardService.replyWrite(replyDTO);
+	}
+	
+	/**
+     * 리플 목록 조회
+     * @param boardNum 본문 글번호
+     * @return  리플 목록
+     */
+	@GetMapping("replyList")
+	public ResponseEntity<?> getReplyList(@RequestParam("boardNum") int boardNum){
+		List<ReplyDTO> replyList 
+			= boardService.getReplyList(boardNum);
+		
+		return ResponseEntity.ok(replyList);
+	}
+	
+	/**
+     * 리플 삭제
+     * @param replyDTO 삭제할 리플 정보
+     * @param user  로그인한 사용자정보
+     */
+	@PostMapping("replyDelete")
+	public void replyDelete(ReplyDTO replyDTO,
+			@AuthenticationPrincipal AuthenticatedUser user){
+		
+		replyDTO.setMemberId(user.getUsername());
+		
+		boardService.replyDelete(replyDTO);
 	}
 
 	
